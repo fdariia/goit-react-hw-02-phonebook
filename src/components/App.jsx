b8401ca4d8caddd0180eac068a23fec72b2166b5
fdariia <fdariya3005@gmail.com>
2023-02-28 23:03:48 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -44,7 +44,7 @@ class App extends Component {
   };
 
   render() {
-    const { contacts, filter } = this.state;
+    const { filter } = this.state;
 
     const filteredContacts = this.getVisibleContacts();
 
