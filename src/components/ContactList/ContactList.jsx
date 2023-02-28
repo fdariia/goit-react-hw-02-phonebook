d113591cb45c8fc8f6a5570c4cb59f9af445189d
fdariia <fdariya3005@gmail.com>
2023-02-28 23:01:07 +0200

    add propTypes

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,13 +1,26 @@
+import PropTypes from 'prop-types';
+
 const ContactList = ({ contacts, onDeleteContact }) => (
   <ul>
-    {contacts.map(({id, name, number}) => (
+    {contacts.map(({ id, name, number }) => (
       <li key={id}>
         <span>{name}</span>
         <span>{number}</span>
-        <button onClick={()=>onDeleteContact(id)}>Delete</button>
+        <button onClick={() => onDeleteContact(id)}>Delete</button>
       </li>
     ))}
   </ul>
 );
+
+ContactList.propTypes = {
+  contacts: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      name: PropTypes.string.isRequired,
+      number: PropTypes.string.isRequired,
+    })
+  ),
+  onDeleteContact: PropTypes.func.isRequired,
+};
 
 export default ContactList;
