e374f93af0fdf44492fa06444b38978920de170f
fdariia <fdariya3005@gmail.com>
2023-03-01 14:38:06 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -8,10 +8,10 @@ import Filter from './Filter';
 class App extends Component {
   state = {
     contacts: [
-      {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
-      {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
-      {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
-      {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
     ],
     filter: '',
   };
@@ -19,7 +19,7 @@ class App extends Component {
   addContact = ({ name, number }) => {
     const newContact = { id: nanoid(), name, number };
 
-    for (const contact of this.state.contacts){
+    for (const contact of this.state.contacts) {
       if (name === contact.name) {
         return alert(`${name} is already in contacts`);
       }
@@ -28,8 +28,10 @@ class App extends Component {
   };
 
   deleteContact = contactId => {
-    this.setState(({contacts}) => ({contacts: contacts.filter(contact => contact.id !== contactId)}))
-  }
+    this.setState(({ contacts }) => ({
+      contacts: contacts.filter(contact => contact.id !== contactId),
+    }));
+  };
 
   changeFilter = evt => {
     this.setState({ filter: evt.currentTarget.value });
@@ -55,7 +57,10 @@ class App extends Component {
 
         <h2>Contacts</h2>
         <Filter value={filter} onChange={this.changeFilter} />
-        <ContactList contacts={filteredContacts} onDeleteContact={this.deleteContact}/>
+        <ContactList
+          contacts={filteredContacts}
+          onDeleteContact={this.deleteContact}
+        />
       </div>
     );
   }
